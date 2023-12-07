bc8c7939c047bb01fe20abf2604d30418fe4ba6e
Wagner.Wang <Wagner.Wang@ASIA35589>
2023-12-08 01:14:36 +0800

    Add project files.

---
+++ Run.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubModuleChild
+{
+    public class Run
+    {
+        public static int Run1()
+        {
+            return 3;
+        }
+    }
+}
